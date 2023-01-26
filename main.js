018d0936897cad51bd1c0e71d5f8be94aa75883a
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-01-26 11:04:23 -0500

    mobile design and javascript first solution

---
+++ main.js
@@ -0,0 +1,100 @@
+const bill = document.querySelector("#bill");
+const tipAmount = document.querySelector(".tip-amount");
+const totalAmount = document.querySelector(".total-amount");
+const people = document.querySelector("#people");
+const buttons = document.querySelectorAll(".percentage");
+const resetButton = document.querySelector(".reset-button");
+const peopleError = document.querySelector(".people-error-message");
+const billError = document.querySelector(".bill-error-message");
+const custom = document.querySelector(".custom");
+
+bill.addEventListener("focus", () => {
+  bill.value = "";
+  billError.classList.remove("visible");
+  bill.classList.remove("error-outline");
+  tipAmount.innerHTML = "$0.00";
+  totalAmount.innerHTML = "$0.00";
+  custom.value = "";
+});
+
+people.addEventListener("focus", () => {
+  people.value = "";
+  peopleError.classList.remove("visible");
+  people.classList.remove("error-outline");
+  tipAmount.innerHTML = "$0.00";
+  totalAmount.innerHTML = "$0.00";
+  custom.value = "";
+});
+
+const calculate = () => {
+  if (
+    people.value > 0 &&
+    people.value - Math.floor(people.value) === 0 &&
+    bill.value >= 0
+  ) {
+    tipAmount.innerHTML = `$${(
+      (billAmount / peopleAmount) *
+      percentage
+    ).toFixed(2)}`;
+    totalAmount.innerHTML = `$${(
+      billAmount / peopleAmount +
+      tipAmountTotal
+    ).toFixed(2)}`;
+  } else if (
+    people.value > 0 &&
+    people.value - Math.floor(people.value) !== 0 &&
+    bill.value >= 0
+  ) {
+    peopleError.classList.add("visible");
+    peopleError.innerHTML = "No Decimals";
+    people.classList.add("error-outline");
+  } else if (
+    people.value > 0 &&
+    people.value - Math.floor(people.value) !== 0 &&
+    bill.value < 0
+  ) {
+    bill.classList.add("error-outline");
+    billError.classList.add("visible");
+    peopleError.classList.add("visible");
+    peopleError.innerHTML = "No Decimals";
+    people.classList.add("error-outline");
+  } else if (people.value > 0 && bill.value < 0) {
+    bill.classList.add("error-outline");
+    billError.classList.add("visible");
+  } else if (people.value <= 0 && bill.value >= 0) {
+    peopleError.classList.add("visible");
+    peopleError.innerHTML = "Can't be zero";
+    people.classList.add("error-outline");
+  } else {
+    bill.classList.add("error-outline");
+    billError.classList.add("visible");
+    peopleError.classList.add("visible");
+    peopleError.innerHTML = "Can't be zero";
+    people.classList.add("error-outline");
+  }
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    percentage = button.value;
+    billAmount = bill.value;
+    peopleAmount = people.value;
+    tipAmountTotal = (billAmount / peopleAmount) * percentage;
+    calculate();
+  });
+});
+
+custom.addEventListener("change", () => {
+  percentage = custom.value / 100;
+  billAmount = bill.value;
+  peopleAmount = people.value;
+  tipAmountTotal = (billAmount / peopleAmount) * percentage;
+  calculate();
+});
+
+resetButton.addEventListener("click", () => {
+  tipAmount.innerHTML = "0.00";
+  totalAmount.innerHTML = "0.00";
+  people.value = "1";
+  bill.value = "0.00";
+});
